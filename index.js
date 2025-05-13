047e46dfc73a546fe4d041d69b88194ed5e38a55
Brenda <brendanjeri2001@gmail.com>
2025-05-12 23:25:13 -0700

    changes

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+/*function scuberGreetingForFeet() {
+  let distance = prompt("Enter distance to be covered:");
+  let fare;
+
+  if (distance <= 400) {
+    fare = "This one is on me!";
+  } else if (distance > 2500) {
+    fare = "No can do";
+  } else if (distance > 2000) {
+    fare = "I will gladly take your thirty bucks.";
+  }
+  else{
+    fare = "invalid";
+  }
+  console.log(scuberGreetingForFeet());
+
+  return fare;
 }
+*/
+console.log("hello");
+
+function scuberGreetingForFeet()
+{
+  let distance =perseInt(prompt("Enter distance to be covered:"));
+  let fare;
+  if (distance <= 400){
+    fare = "This one is on me!";
+  }
+  else if (distance > 2000){
+    fare = "I will gladly take your thirty bucks.";
+  }
+  else if(distance > 2500) {
+    fare = "No can do";
+  }
+  console.log(fare);
+  
+}
+console.log(scuberGreetingForFeet);
 
 function ternaryCheckCity(){
   // Write your code here!
